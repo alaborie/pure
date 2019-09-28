65fa481695d49b2df6c95b72360e954d87b0a6ed
Alexandre Laborie <alaborie@gmail.com>
2019-09-28 08:11:43 -0400

    Remove useless space in git info.

--- pure.zsh
+++ pure.zsh
@@ -156,12 +156,12 @@ prompt_pure_preprompt_render() {
 	typeset -gA prompt_pure_vcs_info
 	if [[ -n $prompt_pure_vcs_info[branch] ]]; then
 		preprompt_parts+=('%F{white}(%f%')
-		preprompt_parts+=('%F{$git_color}%U${prompt_pure_vcs_info[branch]}%u${prompt_pure_git_dirty}%f')
-	fi
-	# Git pull/push arrows.
-	if [[ -n $prompt_pure_git_arrows ]]; then
-		preprompt_parts+=(' %F{$prompt_pure_colors[git:arrow]}${prompt_pure_git_arrows})%f')
-	elif [[ -n $prompt_pure_vcs_info[branch] ]]; then
+		preprompt_parts+=("%F{$git_color}"'%U${prompt_pure_vcs_info[branch]}%u${prompt_pure_git_dirty}%f%')
+
+		# Git pull/push arrows.
+		if [[ -n $prompt_pure_git_arrows ]]; then
+			preprompt_parts+=(' %F{$prompt_pure_colors[git:arrow]}${prompt_pure_git_arrows}%f%')
+		fi
 		preprompt_parts+=('%F{white})%f')
 	fi
 
